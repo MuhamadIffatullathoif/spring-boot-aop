99c2ca04f0210ae17a5a53d7dfb3242e93613b99
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-23 07:49:23 +0700

    03. Create the aspect component

---
+++ src/main/java/com/iffat/springboot/aop/aop/GreetingAspect.java
@@ -0,0 +1,24 @@
+package com.iffat.springboot.aop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class GreetingAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(String com.iffat.springboot.aop.services.GreetingService.sayHello(..))")
+    public void loggerBefore(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Method: {}, args: {}", method, args);
+    }
+}
